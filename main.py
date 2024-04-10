aeeb2c80920b0ec7ff8b80191af7dd6ca9082403
nirvso <sonirvana313@gmail.com>
2024-04-10 15:33:46 -0400

    #comment wooo

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 
 def encode(num):
+    new_number = ""
+    for digit in str(num):
+        add_three = (digit + 3)
+        add_three += new_number
+    return new_number
+
+print(encode(123))
+
+def decode(num):
     pass
 
-if __name__ == '__main__':
+"""
+def main():
     while True:
-        #menu
-    pass
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = int(input("Please enter your password to encode: "))
+            encode(password)
+
+
+if __name__ == '__main__':
+    main()
+"""
