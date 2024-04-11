dc3bb296bd8c9eef9282b0d82057c460c4129182
nirvso <sonirvana313@gmail.com>
2024-04-11 18:37:14 -0400

    encode(num) function that uses a for loop to go through each digit in num and add three to each of the digits then returns the new number.

--- main.py
+++ main.py
@@ -2,31 +2,8 @@
 def encode(num):
     new_number = ""
     for digit in str(num):
-        add_three = (digit + 3)
-        add_three += new_number
+        add_three = str(int(digit) + 3)
+        new_number += add_three
     return new_number
 
-print(encode(123))
 
-def decode(num):
-    pass
-
-"""
-def main():
-    while True:
-        print("Menu")
-        print("-------------")
-        print("1. Encode")
-        print("2. Decode")
-        print("3. Quit")
-
-        option = int(input("Please enter an option: "))
-
-        if option == 1:
-            password = int(input("Please enter your password to encode: "))
-            encode(password)
-
-
-if __name__ == '__main__':
-    main()
-"""
